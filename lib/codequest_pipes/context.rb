bfd8fc3be3c8bedeaeb518e3b84f6ffd42593f02
Marcin Wyszynski <marcin.pixie@gmail.com>
2015-11-30 10:23:46 +0100

    Explicit pipe termination

--- lib/codequest_pipes/context.rb
+++ lib/codequest_pipes/context.rb
@@ -3,15 +3,22 @@ module Pipes
   # OpenStruct except you can write a value only once - this way we prevent
   # context keys from being overwritten.
   class Context
+    attr_reader :error
+
     # Override is an exception raised when an attempt is made to override an
     # existing Context property.
     class Override < ::StandardError; end
+
+    # ExecutionTerminated is an exception raised when the `fail` method is
+    # explicitly called on the Context. This terminates the flow of a pipe.
+    class ExecutionTerminated < ::StandardError; end
 
     # Context constructor.
     #
     # @param values [Hash]
     def initialize(values = {})
       add(values)
+      @error = nil
     end
 
     # Method `add` allows adding new properties (as a Hash) to the Context.
@@ -23,6 +30,32 @@ module Pipes
         fail Override, "Property :#{key} already present" if respond_to?(k_sym)
         define_singleton_method(k_sym) { val }
       end
+    end
+
+    # Explicitly fail the pipe, allowing the error to be saved and accessed from
+    # the Context.
+    #
+    # @param error [Any] Error to be set.
+    #
+    # @raise [ExecutionTerminated]
+    def terminate(error)
+      @error = error
+      fail ExecutionTerminated, error
+    end
+
+    # Check if the Context finished successfully.
+    # This method smells of :reek:NilCheck
+    #
+    # @return [Boolean] Success status.
+    def success?
+      @error.nil?
+    end
+
+    # Check if the Context failed.
+    #
+    # @return [Boolean] Failure status.
+    def failure?
+      !success?
     end
   end # class Context
 end # module Pipes
